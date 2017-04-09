fead77cc1f213cf4d218f11576d538822d1ed6f8
ADL175 <armydavidlim@gmail.com>
2017-04-09 16:41:23 -0700

    App fully functioning

---
+++ CFS3ToDoList/NewTodoViewController.swift
@@ -0,0 +1,46 @@
+//
+//  NewTodoViewController.swift
+//  CFS3ToDoList
+//
+//  Created by David Lim on 4/9/17.
+//  Copyright © 2017 Adam Wallraff. All rights reserved.
+//
+
+import UIKit
+
+class NewTodoViewController: UIViewController, UITextFieldDelegate {
+
+    @IBOutlet weak var textField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.textField.delegate = self
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func closeButtonPressed(_ sender: AnyObject) {
+        
+        dismiss(animated: true, completion: nil)
+
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        if let userText = textField.text{
+            let todo = Todo(text: userText)
+            TodoList.shared.add(todo: todo)
+        }
+        
+        dismiss(animated: true, completion: nil)
+        
+        return true
+    }
+
+
+}
--- CFS3ToDoList/ViewController.swift
+++ CFS3ToDoList/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController, UITableViewDataSource {
 
     
     @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -28,6 +29,12 @@ class ViewController: UIViewController, UITableViewDataSource {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        tableView.reloadData()
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
